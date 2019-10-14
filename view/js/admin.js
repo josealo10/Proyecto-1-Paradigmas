d876ba17cd276c906d7c8b295eb1f92127625205
Alessandro Fazio <alessandro.fazio.perez@gmail.com>
2019-10-14 13:38:43 -0600

    about

--- view/js/admin.js
+++ view/js/admin.js
@@ -1,18 +1,17 @@
-
 var file
 
-function seleccionarRuta(){
-    let archivoInput = document.getElementById('archivo').files[0]    
+function seleccionarRuta() {
+    let archivoInput = document.getElementById('archivo').files[0]
     alert(archivoInput.name)
 
-    $.ajax({ 
-        type: "POST", 
-        contentType: "multipart/form-data", 
+    $.ajax({
+        type: "POST",
+        contentType: "multipart/form-data",
         data: archivoInput,
-        url: "http://localhost:9001/admin" 
+        url: "http://localhost:9001/admin"
     })
 }
 
-function respuesta(res){
-    aler(res)
+function respuesta(res) {
+    alert(res)
 }
