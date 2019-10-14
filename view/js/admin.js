65040781fa7f0d300f3b3406306c05960f369631
Unknown <josealonso.alfa@gmail.com>
2019-10-14 14:21:49 -0600

    post desde el admin

--- view/js/admin.js
+++ view/js/admin.js
@@ -1,18 +1,40 @@
 
-var file
+let flag = false
 
 function seleccionarRuta(){
-    let archivoInput = document.getElementById('archivo').files[0]    
-    alert(archivoInput.name)
-
-    $.ajax({ 
-        type: "POST", 
-        contentType: "multipart/form-data", 
-        data: archivoInput,
-        url: "http://localhost:9001/admin" 
-    })
+    let file = document.getElementById('archivo').files[0]    
+    let lable = document.getElementById("lable")
+    lable.textContent = file.name
+    flag = true
 }
 
 function respuesta(res){
     aler(res)
+}
+
+function enviar(){
+    if(flag){
+        let file = document.getElementById('archivo').files[0]
+        var formData = new FormData()
+        formData.append("rive",file)
+
+
+        $.ajax({
+            url: "http://localhost:9001/admin",
+            type: "post",
+            dataType: "html",
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(data, status, xhttp)
+                {  
+                    if ( data )
+                    {
+                        alert(JSON.stringify(data))
+                    }
+                }
+        })
+    }
+    
 }
